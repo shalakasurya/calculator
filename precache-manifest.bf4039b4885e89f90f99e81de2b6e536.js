12f562875e72c878b62702b11064ceb1775c3bb6
shalakasurya <32623422+shalakasurya@users.noreply.github.com>
2022-04-25 13:20:53 -0400

    Updates

---
+++ precache-manifest.bf4039b4885e89f90f99e81de2b6e536.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9690214ae0d1b55d090",
+    "url": "/calculator/static/css/main.2bd68186.chunk.css"
+  },
+  {
+    "revision": "c9690214ae0d1b55d090",
+    "url": "/calculator/static/js/main.c9690214.chunk.js"
+  },
+  {
+    "revision": "17fb29ed40cd8c1083bf",
+    "url": "/calculator/static/js/1.17fb29ed.chunk.js"
+  },
+  {
+    "revision": "179a28bc762de53c1387",
+    "url": "/calculator/static/js/2.179a28bc.chunk.js"
+  },
+  {
+    "revision": "1ff308159320bf8c74f2",
+    "url": "/calculator/static/js/runtime~main.1ff30815.js"
+  },
+  {
+    "revision": "b1a5acab029a40ccf18b55a8130a009b",
+    "url": "/calculator/index.html"
+  }
+];
